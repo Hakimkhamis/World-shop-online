3e41314b369313933a11e4d900d64e32f80b2c2f
Hakimkhamis <abdulhakim.khamis@stu.ibu.edu.ba>
2018-05-30 00:38:35 +0530

    Dashboard Apply

--- client/app/components/dashboard/dashboard.component.js
+++ client/app/components/dashboard/dashboard.component.js
@@ -1,5 +1,22 @@
 const dashboardController = function($scope, databaseService, $location) {
-   
+    $scope.orders = [];
+    $scope.pendingorder = 0;
+    $scope.totalearning = 0;
+    function loadOrders() {
+        $scope.orders = [];
+        databaseService.getFromDatabase('/api/getorder')
+            .then((orders) => {
+                $scope.orders = orders;
+                angular.forEach($scope.orders, function(item){
+                    $scope.totalearning = $scope.totalearning + item.price;
+                    if(item.status == 'open'){
+                        $scope.pendingorder = $scope.pendingorder + 1;
+                    }
+                })
+            });
+    }
+
+    loadOrders();
 };
 
 angular.module('myApp')
